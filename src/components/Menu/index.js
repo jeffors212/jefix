bf8833a58e132385817e8b361f7d2665143cacaa
unknown <jeffors212@gmail.com>
2020-11-16 22:50:15 -0300

    button

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -7,6 +7,11 @@ function Menu (){
             <a href= "/">
             <img className="Logo" src={JEFLIX} alt="estudo logo"/>
             </a>
+            
+            <a className="ButtonLink"href="/">
+                Novo Video
+            </a>
+
         </nav>
 
     );
